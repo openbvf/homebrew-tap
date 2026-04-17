045e30bc5d87b49dbbf9a7c5bd81858427bb2e94
uncertaininference <gituc@newvoll.net>
2026-04-17 15:53:23 -0700

    Bump for version on crates.io

--- Formula/bvf.rb
+++ Formula/bvf.rb
@@ -1,13 +1,13 @@
 class Bvf < Formula
   desc "Command-line tool for encrypting and decrypting files using the bvf format"
   homepage "https://github.com/openbvf/bvf"
-  version "0.1.11"
+  version "0.1.13"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.1.11/bvf-macos-aarch64.tar.gz"
-  sha256 "041a516a4b911d9faf582ef125a49ce8c63a68574cade35c9729f9bd6d39156b"
+  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.1.13/bvf-macos-aarch64.tar.gz"
+  sha256 "2d76b1f2e5690ae4adddb009c55e995e72ef513ed91033c36099a9972de3836e"
 
   def install
     bin.install "bvf"
