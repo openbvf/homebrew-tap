6d1e04b25b1e0d365a07a10a936ff83316efb0f0
uncertaininference <gituc@newvoll.net>
2026-04-20 08:17:51 -0700

    Release 0.2.1

--- Formula/bvf.rb
+++ Formula/bvf.rb
@@ -1,13 +1,13 @@
 class Bvf < Formula
   desc "Command-line tool for encrypting and decrypting files using the bvf format"
   homepage "https://github.com/openbvf/bvf"
-  version "0.1.13"
+  version "0.2.1"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.1.13/bvf-macos-aarch64.tar.gz"
-  sha256 "2d76b1f2e5690ae4adddb009c55e995e72ef513ed91033c36099a9972de3836e"
+  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.2.1/bvf-macos-aarch64.tar.gz"
+  sha256 "d019f9166b6c0fe5740f6453731fcf0873a1eedb4387c1862e3b0e72847ad824"
 
   def install
     bin.install "bvf"
