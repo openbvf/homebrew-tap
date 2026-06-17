bc3852e5c9d6737f10bd5006bed66bb8150bdbce
uncertaininference <gituc@newvoll.net>
2026-06-17 12:39:22 -0700

    Bad hash; really got to automate

--- Formula/bvf.rb
+++ Formula/bvf.rb
@@ -7,7 +7,7 @@ class Bvf < Formula
   depends_on arch: :arm64
 
   url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.2.2/bvf-macos-aarch64.tar.gz"
-  sha256 "5681b15c5df0e9ca4d7a0485eb1a77b4cf287d3bc2040d372814d90933d27508"
+  sha256 "32fc5a816feec04fd1cbdb675ed4ffd4be6aaca424b308f6b8219e8aa17e460d"
 
   def install
     bin.install "bvf"
