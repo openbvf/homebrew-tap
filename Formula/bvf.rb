a2818592f58d4b43bb4107690ee45abf02936218
uncertaininference <gituc@newvoll.net>
2026-06-17 12:09:40 -0700

    Release 0.2.2

--- Formula/bvf.rb
+++ Formula/bvf.rb
@@ -6,8 +6,8 @@ class Bvf < Formula
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.2.1/bvf-macos-aarch64.tar.gz"
-  sha256 "d019f9166b6c0fe5740f6453731fcf0873a1eedb4387c1862e3b0e72847ad824"
+  url "https://github.com/openbvf/bvf/releases/download/bvf-cli-v0.2.2/bvf-macos-aarch64.tar.gz"
+  sha256 "5681b15c5df0e9ca4d7a0485eb1a77b4cf287d3bc2040d372814d90933d27508"
 
   def install
     bin.install "bvf"
